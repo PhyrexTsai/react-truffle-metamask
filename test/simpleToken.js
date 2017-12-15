d61daed3a3e5589512833975b3c2e0e129cfdd03
Yung chieh Tsai <x01ep23i@hotmail.com>
2017-12-15 11:48:17 +0800

    Update SimpleToken contract test

--- test/simpleToken.js
+++ test/simpleToken.js
@@ -24,4 +24,13 @@ contract('SimpleToken', function(accounts) {
       assert.equal(result.toNumber(), 18, "SimpleToken.decimals wasn't correct");
     });
   });
+
+  it("should initial SimpleToken contract", function() {
+    return SimpleToken.deployed().then(function(instance) {
+      return instance.wallet.call();
+    }).then(function(wallet) {
+      assert.equal(wallet, '0x0000000000000000000000000000000000000000', "SimpleToken.wallet wasn't correct");
+      //return instance.initialize();
+    });
+  });
 });
